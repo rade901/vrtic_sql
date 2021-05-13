3724d0226902526d2f1a63b8749e38d927a08c66
Rade Jasenovcanin <78330407+rade901@users.noreply.github.com>
2021-05-13 04:23:43 +0200

    Update vrtic.sql

--- vrtic.sql
+++ vrtic.sql
@@ -10,8 +10,7 @@ sifra int not null primary key auto_increment,
 skupina_1 int not null,
 skupina_2 int not null,
 skupina_3 int not null,
-odgajateljica int not null,
-datum_pocetka date
+odgajateljica int not null
 );
 
 create table odgajateljica(
@@ -26,21 +25,24 @@ sifra int not null primary key auto_increment,
 ime varchar(20)not null,
 prezime varchar(20)not null,
 opis text,
-broj_telefona_roditelja varchar(20)not null
+broj_telefona_roditelja varchar(20)not null,
+datum_pocetka date
 );
 create table skupina_2(
 sifra int not null primary key auto_increment,
 ime varchar(20) not null,
 prezime varchar(20) not null,
 opis text,
-broj_telefona_roditelja varchar(20)not null
+broj_telefona_roditelja varchar(20)not null,
+datum_pocetka date
 );
 create table skupina_3(
 sifra int not null primary key auto_increment,
 ime varchar(20)not null,
 prezime varchar(20)not null,
 opis text,
-broj_telefona_roditelja varchar(20)not null
+broj_telefona_roditelja varchar(20)not null,
+datum_pocetka date
 );
 
 alter table odgojna_skupina add foreign key (odgajateljica) references odgajateljica(sifra);
@@ -53,21 +55,22 @@ alter table odgojna_skupina add foreign key (skupina_3) references skupina_3(sif
 
 insert into odgajateljica (sifra,ime,prezime,strucna_sprema) values (null,'nikolina','cebic','vss');
 
-insert into skupina_1 (sifra,ime,prezime,opis,broj_telefona_roditelja) values (null,'pero','peric','djete treba vise paznje','0983384666');
+insert into skupina_1 (sifra,ime,prezime,opis,broj_telefona_roditelja,datum_pocetka) values (null,'pero','peric','djete treba vise paznje','0983384666','2008-11-11');
 
-insert into skupina_1 (sifra,ime,prezime,opis,broj_telefona_roditelja) values (null,'luka','lukic','djete mora piti ljek','0955663335');
+insert into skupina_1 (sifra,ime,prezime,opis,broj_telefona_roditelja,datum_pocetka) values (null,'luka','lukic','djete mora piti ljek','0955663335','2008-11-11');
 
-insert into skupina_2 (sifra,ime,prezime,opis,broj_telefona_roditelja) values (null,'matej','peric','djete treba vise sna','091777888');
+insert into skupina_2 (sifra,ime,prezime,opis,broj_telefona_roditelja,datum_pocetka) values (null,'matej','peric','djete treba vise sna','091777888','2008-11-19');
 
-insert into skupina_2 (sifra,ime,prezime,opis,broj_telefona_roditelja) values (null,'oliver','lukic','djete mora piti ljek','091555999');
+insert into skupina_2 (sifra,ime,prezime,opis,broj_telefona_roditelja,datum_pocetka) values (null,'oliver','lukic','djete mora piti ljek','091555999','2008-11-19');
 
-insert into skupina_3 (sifra,ime,prezime,opis,broj_telefona_roditelja) values (null,'tomislav','peric','djete treba vise sna','0987771112');
+insert into skupina_3 (sifra,ime,prezime,opis,broj_telefona_roditelja,datum_pocetka) values (null,'tomislav','peric','djete treba vise sna','0987771112','2008-11-28');
 
-insert into skupina_3 (sifra,ime,prezime,opis,broj_telefona_roditelja) values (null,'kruno','lukic','djete mora piti ljek','09866655521');
+insert into skupina_3 (sifra,ime,prezime,opis,broj_telefona_roditelja,datum_pocetka) values (null,'kruno','lukic','djete mora piti ljek','09866655521','2008-11-28');
 
 
-insert into odgojna_skupina (sifra,skupina_1,skupina_2,skupina_3,odgajateljica,datum_pocetka) values (null,1,1,1,1,'2008-11-11');
+insert into odgojna_skupina (sifra,skupina_1,skupina_2,skupina_3,odgajateljica) values (null,1,1,1,1);
 
-insert into odgojna_skupina (sifra,skupina_1,skupina_2,skupina_3,odgajateljica,datum_pocetka) values (null,2,2,2,1,'2008-11-11');
+insert into odgojna_skupina (sifra,skupina_1,skupina_2,skupina_3,odgajateljica) values (null,2,2,2,1);
+
 
 
